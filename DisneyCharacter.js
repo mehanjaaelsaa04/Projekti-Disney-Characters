ffa7b74bcf43c4ed2e967857ac48f364f2f18cf8
mehanjaaelsaa04 <149594553+mehanjaaelsaa04@users.noreply.github.com>
2023-12-10 19:23:54 +0100

    Update DisneyCharacter.js

--- DisneyCharacter.js
+++ DisneyCharacter.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function){
+  
+  var characterImages = document.querySelectorAll('.box img');
 
+  characterImages.forEach(function(image)){
+    image.addEventListener('mouseover', function(){
+      image.style.border = '2px solid red';
+    });
+
+     image.addEventListener('mouseover', function(){
+      image.style.border = '2px solid black';
+    });
+    
+  });
+});
