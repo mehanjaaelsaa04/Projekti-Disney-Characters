de11e720ff909effc26b15a2c631ac573997775a
mehanjaaelsaa04 <149594553+mehanjaaelsaa04@users.noreply.github.com>
2024-02-02 20:30:23 +0100

    Update DisneyCharacter.js

--- DisneyCharacter.js
+++ DisneyCharacter.js
@@ -1,3 +1,45 @@
+var slideIndex = 1;
+showSlides();
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlides(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots =document.getElementsByClassName("dot");
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className =  dots[i].className.replace("active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += "active";
+    
+
+    setTimeout(function (){
+        plusSlides(1);
+    }, 2000);
+}
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
 
     var characterImages = document.querySelectorAll('.box img');
@@ -13,3 +55,4 @@ document.addEventListener("DOMContentLoaded", function () {
   
     });
 });
+  
