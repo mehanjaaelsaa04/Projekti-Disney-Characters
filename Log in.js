8fe08993cb3fd63711b705582e0598bf76b820d9
mehanjaaelsaa04 <149594553+mehanjaaelsaa04@users.noreply.github.com>
2024-02-02 20:32:51 +0100

    Update Log in.js

--- Log in.js
+++ Log in.js
@@ -1,16 +1,10 @@
 
-     let nameRegex = /^[A-Z][a-z]{2,10}$/;
-     let surnameRegex = /^[A-Z][a-z]{2,10}$/;
      let usernameRegex = /^.{8,15}$/;
      let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
      let passwordRegex = /^[A-Z].*\d{3}$/;
      
 
      function validateForm(){
-         let nameInput = document.getElementById('name');
-         let nameError = document.getElementById('nameError');
-         let surnameInput = document.getElementById('surname');
-         let surnameError = document.getElementById('surnameError');
          let usernameInput = document.getElementById('username');
          let usernameError = document.getElementById('usernameError');
          let emailInput = document.getElementById('email');
@@ -18,20 +12,10 @@
          let passwordInput = document.getElementById('password');
          let passwordError = document.getElementById('passwordError');
 
-         nameError.innerText = '';
-         surnameError.innerText = '';
          usernameError.innerText = '';
          emailError.innerText = '';
          passwordError.innerText = '';
      
-         if(!nameRegex.test(nameInput.value)){
-             nameError.innerText = 'Name is invalid!';
-             return;
-         }
-         if(!surnameRegex.test(surnameInput.value)){
-             surnameError.innerText = 'Surname is invalid!';
-             return;
-         }
          if(!usernameRegex.test(usernameInput.value)){
              usernameError.innerText = 'Username is invalid!';
              return;
