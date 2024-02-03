e8ec29c16f992377e35b0ba6a2fc2cc80346bd28
mehanjaaelsaa04 <149594553+mehanjaaelsaa04@users.noreply.github.com>
2024-02-03 22:23:09 +0100

    Update Log in.js

--- Log in.js
+++ Log in.js
@@ -1,25 +1,17 @@
-
-     let usernameRegex = /^.{8,15}$/;
-     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-     let passwordRegex = /^[A-Z].*\d{3}$/;
+let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+let passwordRegex = /^[A-Z].*\d{3}$/;
      
 
      function validateForm(){
-         let usernameInput = document.getElementById('username');
-         let usernameError = document.getElementById('usernameError');
          let emailInput = document.getElementById('email');
          let emailError = document.getElementById('emailError');
          let passwordInput = document.getElementById('password');
          let passwordError = document.getElementById('passwordError');
 
-         usernameError.innerText = '';
          emailError.innerText = '';
          passwordError.innerText = '';
      
-         if(!usernameRegex.test(usernameInput.value)){
-             usernameError.innerText = 'Username is invalid!';
-             return;
-         }
+
          if(!emailRegex.test(emailInput.value)){
              emailError.innerText = 'Email is invalid!';
              return;
