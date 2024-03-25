48f205e124b510d55c4409da15a838d001f7e8a1
mehanjaaelsaa04 <149594553+mehanjaaelsaa04@users.noreply.github.com>
2024-03-25 22:48:10 +0100

    Update Create an account.php

--- Create an account.php
+++ Create an account.php
@@ -1,26 +1,32 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $Name = $_POST['name'];
-    $Surname = $_POST['surname'];
-    $Email = $_POST['email'];
-    $Username = $_POST['username'];
-    $Password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $conn = new mysqli('localhost', 'root', '', 'webi');
+    $conn = new mysqli('localhost', 'root', '', 'projekt');
     if ($conn->connect_error) {
         die('Connection Failed: ' . $conn->connect_error);
     } else {
         $stmt = $conn->prepare("INSERT INTO registration (Name, Surname, Email, Username, Password)
                 VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssss", $Name, $Surname, $Email, $Username, $Password);
-        
-        if ($stmt->execute()) {
+        $stmt->bind_param("sssss", $name, $surname, $email, $username, $password);
+		
+		if ($stmt->execute()) {
             echo "Registration Successfully...";
         } else {
             echo "Error during registration: " . $stmt->error;
         }
 
+
+        $stmt->execute();
+
+      
         $stmt->close();
+        
+        
         $conn->close();
     }
 }
